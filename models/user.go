df10b6d7f5d0233e6924bfbb6f41e99fe203c5fb
shivam durgbuns <shivamdurgbuns@gmail.com>
2021-03-10 19:43:14 +0530

    Added GetUser and AddUser function to user model.

--- models/user.go
+++ models/user.go
@@ -10,6 +10,23 @@ type User struct {
 }
 
 var (
-	user   []*User
+	users  []*User
 	nextID = 1
 )
+
+/*
+GetUsers function will get all the users saved.
+*/
+func GetUsers() []*User {
+	return users
+}
+
+/*
+AddUser will add a new user.
+*/
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+	return u, nil
+}
