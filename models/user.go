d130a4175d5d08ec8eb982be9aae517fac413798
shivam durgbuns <shivamdurgbuns@gmail.com>
2021-03-11 20:33:25 +0530

    Added GetUserByID, UpdateUserByID, RemoveUserByID function in models file.

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 /*
 User is the structre of a user.
 */
@@ -25,8 +30,49 @@ func GetUsers() []*User {
 AddUser will add a new user.
 */
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("New users must not include ID or it must be equal to 0")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+/*
+GetUserByID function returns the user info wrt to the id given as parameter.
+*/
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID %v was not found", id)
+}
+
+/*
+UpdateUser functino will update the information of the user.
+*/
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID %v was not found", u.ID)
+}
+
+/*
+RemoveUserByID function will remove the user by ID given in parameter.
+*/
+func RemoveUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID %v not found", id)
 }
