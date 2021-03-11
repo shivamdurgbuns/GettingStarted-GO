059197190dc5ec4cab756718ab0d72f7c044615f
shivam durgbuns <shivamdurgbuns@gmail.com>
2021-03-11 10:43:19 +0530

    Addeed front.go file and registered new controller.

---
+++ controllers/user.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"net/http"
+	"regexp"
+)
+
+type userController struct {
+	userIDPattern *regexp.Regexp
+}
+
+func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hell from the User Controller."))
+}
+
+func newUserController() *userController {
+	return &userController{
+	userIDPattern: regexp.MustCompile('^/users/(\d+)/?'),
+}
+}
+
