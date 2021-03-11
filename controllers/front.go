cd114ad11eaf61556b336e370c8c812c1da0a02c
shivam durgbuns <shivamdurgbuns@gmail.com>
2021-03-11 20:35:20 +0530

    Added JSON encoder function

--- controllers/front.go
+++ controllers/front.go
@@ -1,10 +1,22 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
+/*
+RegisterController function will register all http handels.
+*/
 func RegisterController() {
 	uc := newUserController()
 
 	http.Handle("/users", *uc)
 	http.Handle("/users/", *uc)
+}
+
+func encodeResponseAsJSON(data interface{}, w io.Writer) {
+	enc := json.NewEncoder(w)
+	enc.Encode(data)
 }
